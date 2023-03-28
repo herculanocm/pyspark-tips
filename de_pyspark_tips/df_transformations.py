b4955a597c564944f012a3a1a11ed124ea5621d8
Herculano Cunha <herculanocm@outlook.com>
2023-03-28 14:39:09 -0300

    df transformation

--- de_pyspark_tips/df_transformations.py
+++ de_pyspark_tips/df_transformations.py
@@ -7,6 +7,7 @@ import logging
 from sys import stdout
 from de_pyspark_tips.custom_formatter_logger import CustomFormatter
 
+
 def get_logger(logger: logging.Logger = None) -> logging.Logger:
     if logger is None:
 
@@ -20,67 +21,127 @@ def get_logger(logger: logging.Logger = None) -> logging.Logger:
 
     return logger
 
+
+@F.udf(returnType=StringType())
+def check_date_format_v1(date_string, format="%Y-%m-%d"):
+
+    if date_string is None:
+        return None
+
+    from datetime import datetime
+
+    valid = False
+    try:
+        datetime_converted = datetime.strptime(date_string, format)
+        valid = True
+    except ValueError:
+        valid = False
+
+    datetime_compare = datetime(1900, 1, 1)
+    if valid and datetime_converted > datetime_compare:
+        return datetime_converted.date().strftime(format)
+    else:
+        return datetime_compare.strftime(format)
+
+
+@F.udf(returnType=StringType())
+def check_datetime_format_v1(date_string, format='%Y-%m-%d %H:%M:%S.%f'):
+
+    if date_string is None:
+        return None
+
+    from datetime import datetime
+
+    valid = False
+    try:
+        datetime_converted = datetime.strptime(date_string, format)
+        valid = True
+    except ValueError:
+        valid = False
+
+    if valid == False:
+        format_2 = '%Y-%m-%d %H:%M:%S'
+        try:
+            datetime_converted = datetime.strptime(date_string, format_2)
+            valid = True
+        except ValueError:
+            valid = False
+
+    datetime_compare = datetime(1900, 1, 1, 23, 59, 59, 999999)
+    if valid and datetime_converted > datetime_compare:
+        return datetime_converted.strftime(format)
+    else:
+        return datetime_compare.strftime(format)
+
+
 def cast_columns_types_by_schema(df: DataFrame, list_schema: list, empty_columns_to_none: bool = False, logger=None) -> DataFrame:
     logger = get_logger(logger)
     if (df is None or list_schema is None) or (len(list_schema) == 0):
         raise ValueError('The values could by not null or empty')
-    else: 
+    else:
         for column in list_schema:
             fieldname = column['column_name']
             fieldtype = column['data_type'].lower()
 
-            logger.info(f"Casting column : {column['column_name']} to data_type: {column['data_type']}")
+            logger.info(
+                f"Casting column : {column['column_name']} to data_type: {column['data_type']}")
 
             if 'int' in fieldtype and 'big' not in fieldtype:
-                df = df.withColumn(fieldname, F.col(fieldname).cast(IntegerType()))
+                df = df.withColumn(fieldname, F.col(
+                    fieldname).cast(IntegerType()))
 
             elif 'bool' in fieldtype:
-                df = df.withColumn(fieldname, F.col(fieldname).cast(BooleanType()))
-            
+                df = df.withColumn(fieldname, F.col(
+                    fieldname).cast(BooleanType()))
+
             elif 'numeric' in fieldtype or 'decimal' in fieldtype or \
                 'double' in fieldtype or 'float' in fieldtype or \
                 'real' in fieldtype or 'money' in fieldtype or \
-                'currency' in fieldtype:
-                df = df.withColumn(fieldname, F.regexp_replace(F.col(fieldname), ",", '.'))\
-                    .withColumn(fieldname, F.col(fieldname).cast(DoubleType()))
+                    'currency' in fieldtype:
+                df = df.withColumn(fieldname, F.col(fieldname).cast(DoubleType()))
 
             elif 'date' == fieldtype:
-                df = df.withColumn(fieldname,
-                F.when(F.col(fieldname).like('%/%/%'), F.to_date(F.col(fieldname), 'dd/MM/yyyy'))
-                .otherwise(F.date_format(F.col(fieldname), "yyyy-MM-dd")).cast(DateType())
-                )
-                    
+                df = df.withColumn(fieldname, F.col(fieldname).cast(DateType()))
+
             elif 'datetime' == fieldtype or 'timestamp' in fieldtype:
                 df = df.withColumn(fieldname, F.col(fieldname).cast(TimestampType()))
 
             else:
                 df = df.withColumn(fieldname, F.col(fieldname).cast(StringType())) \
-                .withColumn(fieldname, F.regexp_replace(F.col(fieldname),"[\\r\\n]", '')) \
-                .withColumn(fieldname, F.trim(F.col(fieldname)))
+                    .withColumn(fieldname, F.regexp_replace(F.col(fieldname), "[\\r\\n]", '')) \
+                    .withColumn(fieldname, F.trim(F.col(fieldname)))
 
                 if empty_columns_to_none:
-                    df = df.withColumn(fieldname, F.when(F.col(fieldname)=="",None).otherwise(F.col(fieldname)))
+                    df = df.withColumn(fieldname, F.when(
+                        F.col(fieldname) == "", None).otherwise(F.col(fieldname)))
 
         return df
+
 
 def choose_last_row_modify_by_ids(df: DataFrame, list_columns_primary_key: list, list_columns_order: list, logger=None) -> DataFrame:
     logger = get_logger(logger)
     if (df is None or list_columns_primary_key is None or list_columns_order is None) or (len(list_columns_primary_key) == 0 or len(list_columns_order) == 0):
         raise ValueError('The values could by not null or empty')
     else:
-        logger.info(f"Creating partitionBy {','.join(list_columns_primary_key)}")
-        partition_order = Window.partitionBy(*list_columns_primary_key).orderBy(*[F.col(lc).desc() for lc in list_columns_order])
+        logger.info(
+            f"Creating partitionBy {','.join(list_columns_primary_key)}")
+        partition_order = Window.partitionBy(
+            *list_columns_primary_key).orderBy(*[F.col(lc).desc() for lc in list_columns_order])
         logger.info("Filtering rn column")
-        df_filter = df.withColumn('rn', F.row_number().over(partition_order)).filter('rn = 1').drop('rn')
+        df_filter = df.withColumn('rn', F.row_number().over(
+            partition_order)).filter('rn = 1').drop('rn')
         return df_filter
+
 
 def rename_columns_by_list(df: DataFrame, list_columns: List, logger=None) -> DataFrame:
     logger = get_logger(logger)
     for rw in list_columns:
         if rw['column_name'] in df.columns:
-            logger.info(f"renaming column: {rw['column_name']} to {rw['new_name']}")
+            logger.info(
+                f"renaming column: {rw['column_name']} to {rw['new_name']}")
             df = df.withColumnRenamed(rw['column_name'], rw['new_name'])
     return df
+
 
 def format_df(df: DataFrame, schema: Dict) -> DataFrame:
     columns_df = [s['column_name'] for s in schema['columns']]
@@ -88,12 +149,14 @@ def format_df(df: DataFrame, schema: Dict) -> DataFrame:
     df = cast_columns_types_by_schema(df, schema['columns'], True)
     return df
 
+
 def drop_columns_by_list(df: DataFrame, list_columns: list, logger=None) -> DataFrame:
     logger = get_logger(logger)
     for df_col in [df_col for df_col in df.columns if df_col not in list_columns]:
         logger.info(f"drop column: {df_col}")
         df = df.drop(F.col(df_col))
     return df
+
 
 def add_columns_by_list(df: DataFrame, list_columns: list, logger=None) -> DataFrame:
     logger = get_logger(logger)
--- test/test_df_transformations.py
+++ test/test_df_transformations.py
@@ -11,10 +11,10 @@ data_df1 = [
     (1,"2020-08-28", "true", "G1 | ESCADA 1O P/ 2O | STAND | FRALDÁRIO","d", "3.15868", "","I","2020-08-27 23:59:59"),
     (1,"2020-08-28", "true", "G1 | ESCADA 1O P/ 2O | STAND | FRALDÁRIO","d", "3.15868", "","U" ,"2020-08-28 12:59:59"),
     (2,"2018-05-25", "true","G1 | ESCADA 1O P/     2O | STAND | FRALDÁRIO     ","A", "5.5", "NÃO EXISTE", "I","2020-08-27 23:59:59"),
-    (3,"2018-05-25", "true","""G1 | ESCADA 1O P/     2O | STAND | FRALDÁRIO     
+    (3,"1510-05-25", "true","""G1 | ESCADA 1O P/     2O | STAND | FRALDÁRIO     
     NOVO |
     STUFF
-    ""","d", "389.390","", "I","2020-08-27 23:59:59")
+    ""","d", "389.390","", "I","1510-08-27 23:59:59")
     ]
 columns_df1 =["id","data", "ativo", "unidade", "sigla", "valor", "obs", "action", "data_transaction"]
 
@@ -33,7 +33,7 @@ def spark():
     spark.stop()
 
 
-def test_cast_columns_types_by_schema(spark: SparkSession):
+def ttest_cast_columns_types_by_schema(spark: SparkSession):
     print('')
     print('Original DF')
     df=spark.createDataFrame(data_df1, columns_df1)
@@ -63,7 +63,7 @@ def test_cast_columns_types_by_schema(spark: SparkSession):
     assert list_expected_dtypes == list_df_dtypes
 
 
-def test_choose_last_row_modify_by_ids(spark: SparkSession):
+def ttest_choose_last_row_modify_by_ids(spark: SparkSession):
     df=spark.createDataFrame(data_df1, columns_df1)
 
     list_schema = [
@@ -84,5 +84,28 @@ def test_choose_last_row_modify_by_ids(spark: SparkSession):
     filterDF.show(truncate=False)
 
     assert filterDF.filter("id == 1").count() == 1
+
+
+def test_dates(spark: SparkSession):
+    df=spark.createDataFrame(data_df1, columns_df1)
+
+    list_schema = [
+        {'column_name': 'id', 'data_type': 'INTEGER'},
+        {'column_name': 'data', 'data_type': 'DATE'},
+        {'column_name': 'ativo', 'data_type': 'BOOLEAN'},
+        {'column_name': 'unidade', 'data_type': 'VARCHAR(200)'},
+        {'column_name': 'sigla', 'data_type': 'VARCHAR(2)'},
+        {'column_name': 'valor', 'data_type': 'NUMERIC(10,2)'},
+        {'column_name': 'obs', 'data_type': 'VARCHAR(MAX)'},
+        {'column_name': 'action', 'data_type': 'VARCHAR(1)'},
+        {'column_name': 'data_transaction', 'data_type': 'TIMESTAMP'},
+    ]
+
+    castedDF = df_transformations.cast_columns_types_by_schema(df, list_schema, True)
+
+    # filterDF = df_transformations.choose_last_row_modify_by_ids(castedDF, ['id'], ['data_transaction'])
+    castedDF.show(truncate=False)
+
+    assert 1 == 1
 
     
